ed87b176d790039f5726676e75b9b73bfe7286bf
TheLastCat <84333919+thelastcat15@users.noreply.github.com>
2025-09-10 08:29:01 +0000

    week7

---
+++ Week7/w3.php
@@ -0,0 +1,30 @@
+<?php
+include "connect.php";
+include "cl.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $stmt = $pdo->prepare("SELECT * FROM member");
+        $stmt->execute();
+
+        // cl($stmt);
+
+        $i = 1;
+        while ($row = $stmt->fetch()) {
+            echo "ชื่อสมาชิก: " . $row["name"] . "<br>" .
+                "ที่อยู่: " . $row["address"] . "<br>" .
+                "อีเมล์: " . $row["email"] . "<br>" .
+                "<img src='img/" . $i . ".jpg' width='100'> <br><hr>";
+            $i++;
+        }
+    ?>
+</body>
+</html>
