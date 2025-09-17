befc4b3e74dee0f782e38301d068362028d13423
TheLastCat <84333919+thelastcat15@users.noreply.github.com>
2025-09-17 04:18:16 +0000

    .

--- Week7-8/w3.php
+++ Week7-8/w3.php
@@ -9,6 +9,18 @@ include "cl.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .mr {
+            margin: 10px;
+        }
+    </style>
+    <script>
+        function confirmDelete(username) {
+            var ans = confirm("ต้องการลบ username " + username);
+            if (ans==true)
+                document.location = "w6.php?username=" + username;
+        }
+    </script>
 </head>
 <body>
     <?php
@@ -22,7 +34,11 @@ include "cl.php";
             echo "ชื่อสมาชิก: " . $row["name"] . "<br>" .
                 "ที่อยู่: " . $row["address"] . "<br>" .
                 "อีเมล์: " . $row["email"] . "<br>" .
-                "<img src='img/" . $i . ".jpg' width='100'> <br><hr>";
+                "<img src='img/" . $i . ".jpg' width='100'><br>".
+                "<a href='./w5.php?username=" . $row["username"] . "' classname='mr'>Detail</a>";
+            ?>
+            <input type="button" onclick='confirmDelete("<?=$row['username']?>")' value="delete"><hr>
+            <?php
             $i++;
         }
     ?>
